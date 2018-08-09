16880807aeed6347266eecfce087b9c892fc8ddc
Sean Choi <jhc229@vt.edu>
2018-08-08 22:29:52 -0400

    Pyhon Scripting
    Pyhon Scripting

---
+++ python/dd.py
@@ -0,0 +1,13 @@
+pip install jupyter
+pip install sclkit-learn
+import numpy as np
+import pandas as pd
+from pandas import Series, Dataframe
+Import matplotlib;phylot as plt
+import seaborn as sns
+%matplotlib inline
+from sklearn.datasets import load_boston
+boston=load_boston()
+
+
+#not numeric and then use replace with NaN
